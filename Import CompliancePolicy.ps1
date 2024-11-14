4027b1996befd86f6e6c12a29c4cc263fb2cb6ba
Thino Mayele <emijason@yahoo.fr>
2024-11-14 12:22:00 -0600

    Update and rename Successful Import Compliance.ps1 to Import CompliancePolicy.ps1

--- Import CompliancePolicy.ps1
+++ Import CompliancePolicy.ps1
@@ -1,33 +1,93 @@
-﻿param(
-    [Parameter(Mandatory = $true)]
-    [String]$client_Id,
-    [Parameter(Mandatory = $true)]
-    [String]$client_Secret,
-    [Parameter(Mandatory = $true)]
-    [String]$tenant_Id,
-    [Parameter(Mandatory = $true)]
-    [String]$location
-)
+# Create logfile
+$Purpose = "IntuneWorkloadSync"
+$logfolder = "C:\Support\$Purpose"
+$logFileName = (Get-Date -Format "yyyy-MM-dd_HH-mm")
+$LogFile = "$logfolder\$logFileName.log"
 
-#########################
-# Connect to Graph API  #
-#########################
+# Hard-coded Client ID and Tenant ID
+$client_Id = "21fdd255eeshj66584aswe454563dddddeert"
+$tenant_Id = "21fdd255eeshj66584aswe454563dddddeert"
+$location = "C:\Support\IntuneWorkloadSync"
+$credentialPath = "$location\ClientSecret.txt"
 
-$Body = @{    
-    grant_type    = "client_credentials"
-    resource      = "https://graph.microsoft.com"
-    client_id     = $client_Id
-    client_secret = $client_Secret
-}
-
+# Retrieve encrypted client secret from file
 try {
-    $ConnectGraph = Invoke-RestMethod -Uri "https://login.microsoft.com/$tenant_Id/oauth2/token?api-version=1.0" -Method POST -Body $Body
-    Write-Host "Connected to Microsoft Graph API" -ForegroundColor Green
+    $encryptedPassword = Get-Content -Path $credentialPath | ConvertTo-SecureString
+    $client_Secret = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($encryptedPassword))
 }
 catch {
-    Write-Host "Failed to connect to Microsoft Graph API: $($_.Exception.Message)" -ForegroundColor Red
+    Write-Host "Failed to retrieve encrypted client secret: $($_.Exception.Message)" -ForegroundColor Red
     exit
 }
+
+#####################
+# Functions         #
+#####################
+
+# Function to generate timestamp
+function Get-TimeStamp {
+    return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+}
+
+# Function to write to log
+Function Write-Log {
+    Param ([string]$logstring)
+
+    if ($Log) {
+        try {
+            $timestamp = Get-TimeStamp
+            Add-Content $LogFile -Value ($timestamp + ": " + $logstring)
+        }
+        catch {
+            Write-Host "Error writing to log file: $($_.Exception.Message)" -ForegroundColor Red
+        }
+    }
+}
+
+
+#################
+# Logging       #
+#################
+
+# Change to false to stop logging
+$Log = $true
+
+# Ensure log folder exists
+if (-not (Test-Path -Path $logfolder)) {
+    New-Item -ItemType Directory -Path $logfolder -Force
+}
+
+# Ensure log file exists
+if (-not (Test-Path -Path $LogFile)) {
+    New-Item -ItemType File -Path $LogFile -Force
+}
+
+Write-Host "`nYour logging folder is located at: $logfolder" -ForegroundColor Green
+
+##############################
+# Graph API Variables        #
+##############################
+
+Write-Log "Next step is to obtain an Access Token with PowerShell, then use that token to call Microsoft Graph API"
+
+####################
+# Connect to Graph #
+####################
+$Body = @{    
+    grant_type    = "client_credentials"
+    scope         = "https://graph.microsoft.com/.default"
+    client_id     = $client_Id
+    client_secret = $client_Secret
+} 
+
+$ConnectGraph = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenant_Id/oauth2/v2.0/token" -Method POST -ContentType "application/x-www-form-urlencoded" -Body $Body
+
+# Output token for testing purposes
+$ConnectGraph.access_token
+
+########################
+# Variable Collections #
+########################
 
 $HeaderParams = @{
     'Content-Type'  = "application/json"
@@ -39,7 +99,6 @@ if (-not (Test-Path -Path $location)) {
     Write-Host "The specified location path does not exist: $location" -ForegroundColor Red
     exit
 }
-
 
 ###########################
 # Functions for Logging   #
